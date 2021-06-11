c6045d3bfbc35ce2e4a25b50ce0a94b8c54654b0
Boston-of-Gilead <Boston-of-Gilead@users.noreply.github.com>
2021-06-11 07:37:27 -0400

    Add files via upload

---
+++ Wware_Excel.ps1
@@ -0,0 +1,15 @@
+﻿$filename = "c:\users\admin\desktop\SecurityCentralSupportedProducts.xlsx"
+$objExcel = New-Object -ComObject Excel.Application
+#$objExcel.DisplayAlerts = $False
+$objWrkBk  = $objExcel.Workbooks.Open($Filename)
+$objExcel.Visible = $True
+# one way of selecting a sheet e.g. 3rd worksheet
+# $objWrkBk.Sheets(3).Select()
+# another way of selecting a sheet e.g. 1st worksheet
+# $objWrkBk.Worksheets.item(1).Select()
+$objWrkSht1 = $objWrkBk.Worksheets.item(1)
+$objWrkSht1.Select()
+# filter on 7th column, with string matching 'ABC'
+$objWrkSht1.Range('$A$nn:$P$mm').AutoFilter(3,'=Supported')
+$objWrkSht1.Range('$A$nn:$P$mm').AutoFilter(1,'>=$thirtyAgo')
+# $A$nn => range first cell position, $P$mm = range last cell position
